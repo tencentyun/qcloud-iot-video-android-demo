457a7fed0f32290cd76ccd2bf1746d48c0d6ee52
neilyhe <228429803@qq.com>
2024-11-07 16:45:15 +0800

    添加p2pinfo信息

--- app/src/main/java/com/example/ivdemo/IPCActivity.kt
+++ app/src/main/java/com/example/ivdemo/IPCActivity.kt
@@ -1,18 +1,28 @@
 package com.example.ivdemo
 
 import android.graphics.SurfaceTexture
+import android.os.Build
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.TextureView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
 import com.example.ivdemo.popup.QualitySettingDialog
 import com.tencent.iot.twcall.R
 import com.tencent.iot.twcall.databinding.ActivityIpcBinding
 import com.tencent.iot.video.device.VideoNativeInterface
+import com.tencent.iot.video.device.annotations.CsChannelType
 import com.tencent.iot.video.device.annotations.StreamType
 import com.tencent.iot.video.device.model.AvDataInfo
+import com.tencent.iot.video.device.model.CsBalanceInfo
 import com.tencent.iotvideo.link.CameraRecorder
 import com.tencent.iotvideo.link.SimplePlayer
+import com.tencent.iotvideo.link.util.copyTextToClipboard
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class IPCActivity : BaseIPCActivity<ActivityIpcBinding>() {
 
@@ -20,6 +30,8 @@ class IPCActivity : BaseIPCActivity<ActivityIpcBinding>() {
     private val cameraRecorder = CameraRecorder()
     private var lastClickTime = 0L
     private var localPreviewSurface: SurfaceTexture? = null
+    private val handler = Handler(Looper.getMainLooper())
+    private val UPDATE_P2P_INFO_TOKEN = "update_p2p_info_token"
 
     private val listener = object : TextureView.SurfaceTextureListener {
         override fun onSurfaceTextureAvailable(surface: SurfaceTexture, width: Int, height: Int) {
@@ -70,6 +82,9 @@ class IPCActivity : BaseIPCActivity<ActivityIpcBinding>() {
             textDevInfo.text =
                 String.format((getString(R.string.text_device_info)), "${productId}_$deviceName")
             textureViewIpc.surfaceTextureListener = listener
+            tvCopy.setOnClickListener {
+                copyTextToClipboard(this@IPCActivity, tvP2pInfo.text.toString().substringAfter(":"))
+            }
 //            btnIpcCall.setOnClickListener {
 //                val time = System.currentTimeMillis()
 //                val timeD = time - lastClickTime
@@ -82,6 +97,42 @@ class IPCActivity : BaseIPCActivity<ActivityIpcBinding>() {
 //                // msg_id 6: 按门铃
 //                VideoNativeInterface.getInstance().sendMsgNotice(6)
 //            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    private fun updateP2pInfo() {
+        val p2pInfo = VideoNativeInterface.getInstance().p2pInfo
+        if (!binding.tvP2pInfo.text.toString().contains(p2pInfo)) {
+            showToast("P2PInfo 已更新")
+        }
+        binding.tvP2pInfo.text = String.format(getString(R.string.text_p2p_info), p2pInfo)
+        handler.postDelayed(taskRunnable, UPDATE_P2P_INFO_TOKEN, 60000)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    private val taskRunnable = Runnable {
+        updateP2pInfo()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacksAndMessages(UPDATE_P2P_INFO_TOKEN)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onOnline(netDateTime: Long) {
+        super.onOnline(netDateTime)
+        lifecycleScope.launch(Dispatchers.Main) {
+            updateP2pInfo()
+            val info = VideoNativeInterface.getInstance()
+                .getCsBalanceInfo(CsChannelType.CS_MULTI_CH1, 1000)
+            handler.postDelayed(taskRunnable, UPDATE_P2P_INFO_TOKEN, 60000)
+            Log.d(
+                "hhh",
+                "info:${info.csDays}   ${info.csType}   ${info.csSwitch}  ${info.freeTrialRemainingSec}"
+            )
+//            binding.tvCloudStorageState.text =info.
         }
     }
 
